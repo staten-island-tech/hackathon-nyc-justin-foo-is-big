085fcfaebdfc0815d49f9f511448b8374053eb5f
Juiceman78 <vincentz78@nycstudents.net>
2025-06-12 10:17:24 -0400

    S

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-        {"name": "Grand Central Terminal", "image": "https://upload.wikimedia.org/wikipedia/commons/0/0b/Grand_Central_Terminal_Main_Concourse_2014.jpg"},
 @app.route('/')
 def index():
     return render_template('index.html')
